b6c122cc5dfb0441833f45c6f744ece9f1a617b5
Never404s <kdsossa@gmail.com>
2023-03-05 00:30:31 -0600

    added table conditional

--- migration.js
+++ migration.js
@@ -3,20 +3,20 @@ const pool = require('./dbConn');
 
 
 pool.query(
-  `CREATE TABLE author (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-);`
-)
+  `CREATE TABLE IF NOT EXISTS author (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+  );`
+);
 
 pool.query(
-    `CREATE TABLE books (
+  `CREATE TABLE IF NOT EXISTS books (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     genre TEXT NOT NULL,
     author_id INTEGER NOT NULL REFERENCES authors(id)
   );`
-)
+);
 
 
 pool.end()
