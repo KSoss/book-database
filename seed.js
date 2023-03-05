8c6526cd919cc32bf81f7ac3a2b7272469707616
Never404s <kdsossa@gmail.com>
2023-03-05 00:11:31 -0600

    error in sql commond fixed

--- seed.js
+++ seed.js
@@ -29,7 +29,7 @@ pool.query('SELECT COUNT(*) FROM author', (err, result) => {
   });
   
   // Insert books into the database
-  pool.query('SELECT COUNT(*) FROM book', (err, result) => {
+  pool.query('SELECT COUNT(*) FROM books', (err, result) => {
     if (err) {
       console.log('Error checking book count:', err);
       return;
@@ -45,7 +45,7 @@ pool.query('SELECT COUNT(*) FROM author', (err, result) => {
         const authors = result.rows;
         // Insert the books into the database with their respective author ids
         pool.query(
-          `INSERT INTO book (name, genre, author_id) VALUES 
+          `INSERT INTO books (name, genre, author_id) VALUES 
           ('Dune', 'Science Fiction', ${authors[0].id}),
           ('Dune Messiah', 'Science Fiction', ${authors[0].id}),
           ('Children of Dune', 'Science Fiction', ${authors[0].id}),
